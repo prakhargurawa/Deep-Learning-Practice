b2cc9f2f40e5db5b3bc67e27c6cb8b658267f0df
Prakhar Gurawa <prakhargurawa@gmail.com>
2019-10-10 00:22:19 +0530

    Create cnn_image_clasifier_dog_cat_keras.py
    Keras :dog/cat classifier

---
+++ CNN_Practice/cnn_image_clasifier_dog_cat_keras.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  9 23:00:54 2019
+
+@author: prakh
+"""
+##https://github.com/llSourcell/how_to_make_an_image_classifier/blob/master/demo.ipynb
+
+import os
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras import optimizers
+
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'D:\\ZDataDogCat\\train'
+validation_data_dir = 'D:\\ZDataDogCat\\validation'
+
+
+
+# used to rescale the pixel values from [0, 255] to [0, 1] interval
+datagen = ImageDataGenerator(rescale=1./255)
+
+# automagically retrieve images and their classes for train and validation sets
+train_generator = datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=16,
+        class_mode='binary')
+
+validation_generator = datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='binary')
+
+
+
+model = Sequential()
+model.add(Convolution2D(32, 3, 3, input_shape=(img_width, img_height,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+
+nb_epoch = 30
+nb_train_samples = 2048
+nb_validation_samples = 832
+
+
+model.fit_generator(
+        train_generator,
+        samples_per_epoch=nb_train_samples,
+        nb_epoch=nb_epoch,
+        validation_data=validation_generator,
+        nb_val_samples=nb_validation_samples)
+
+model.evaluate_generator(validation_generator, nb_validation_samples)
+
+
